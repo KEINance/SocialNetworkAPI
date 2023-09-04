8d90c4a8f6436c027f4feb7b7c917ae573d0d478
KEINance <keinance2@gmail.com>
2023-09-04 12:04:38 -0400

    index and model touch ups

---
+++ Models/Reactions.js
@@ -0,0 +1,22 @@
+const { Schema, Thought, User} = require('mongoose');
+
+
+const reactions = new Schema({
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal)
+    },
+});
+
+
+module.exports = reactions;
