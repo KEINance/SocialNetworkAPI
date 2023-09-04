df9639092ca5b4d37ed3566c20959b27c5426163
KEINance <keinance2@gmail.com>
2023-09-04 11:25:00 -0400

    config

---
+++ Config/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/social-network');
+
+const connectionString = 'mongodb://127.0.0.1:27017/social-network';
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+.then(() => {
+    console.log('Success! You have connected!');
+})
+.catch((error) => {
+    console.error('Could not connect', error);
+});
+
+module.exports = mongoose.connection;
